b1ff432e43697b06d3be024852bebc8406ef5fb4
acaever <acaever@gmail.com>
2021-01-02 01:39:55 +0900

    add setBadgeText

--- time.js
+++ time.js
@@ -1,14 +1,22 @@
 window.onload = clock;
 
-
 function clock(){
    
     var now = new Date();
+    var day = now.getDay(); //0~6 일요일~토요일
     var hour = now.getHours();
-    if ( hour > 12 ){ hour = hour - 12;}
     var min = now.getMinutes();
     var sec = now.getSeconds();    
+        
+    //미쓰백 program alarm
+    if ( day === 6) {    
+        browser.browserAction.setBadgeText({text: "미쓰"});
+     }else{    
+        browser.browserAction.setBadgeText({text: ""});
+    }
 
+    //가영시 special time alarm
+    if ( hour > 12 ){ hour = hour - 12;} //24H -> 12H
     if ( hour === 12 ) { 
         document.getElementById('pp').innerText = "가영시 " + min + "분 " + sec + "초";
     }else if ( hour === 0){
@@ -21,10 +29,10 @@ function clock(){
     pic();
 }
 
-
-
-function pic(){
+//random number & image show
+function pic(){ 
     var ran_num = Math.floor(Math.random() * 24);
     document.getElementById('pi').src = `./image/${ran_num}.jpg`;
 }
-    
+
+
